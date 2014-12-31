1d09bc79bf7f0ecb8ea2f8f7dc03631b19995fd4
Lucas Wiman <lucaswiman@counsyl.com>
2014-12-31 15:43:33 -0800

    Problem 3

---
+++ prolog/problem3.pl
@@ -0,0 +1,24 @@
+% The prime factors of 13195 are 5, 7, 13 and 29.
+%
+% What is the largest prime factor of the number 600851475143 ?
+
+remove_factor(N, Factor, Reduced) :-
+  0 is N mod Factor -> remove_factor(N / Factor, Factor, Reduced)
+  ; Reduced is N
+.
+largest_odd_prime_factor(N, Div, BiggestFactor) :-
+  Next is Div + 2,
+  (
+    Div * Div > N -> BiggestFactor is N
+    ; 0 is N mod Div ->
+      writeln([N, Div]),
+      remove_factor(N, Div, Reduced),
+      (
+        (Reduced is 1, BiggestFactor is Div);
+        largest_odd_prime_factor(Reduced, Next, BiggestFactor)
+      )
+    ; largest_odd_prime_factor(N, Next, BiggestFactor)
+  )
+.
+
+% largest_odd_prime_factor(600851475143, 3, BiggestFactor)
