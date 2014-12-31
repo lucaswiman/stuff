9f0c22d6b3072e9d855beaa6085325168275e674
Lucas Wiman <lucaswiman@counsyl.com>
2014-12-31 01:39:54 -0800

    Problem1

---
+++ prolog/problem1.pl
@@ -0,0 +1,21 @@
+% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+%
+% Find the sum of all the multiples of 3 or 5 below 1000.
+
+applicable(N) :- 0 is N mod 3; 0 is N mod 5.
+
+inc(N, Inc) :-
+  (not(applicable(N)), Inc is 0);
+  (applicable(N), Inc is N)
+.
+
+problem1(N, Solution) :-
+  N > 0,
+  inc(N, Inc),
+  problem1(N-1, LastSolution),
+  Solution is LastSolution + Inc
+.
+
+problem1(N, 0) :- N < 1.
+
+% [problem1]. problem1(999, Solution).
