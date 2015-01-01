e07b7524f380f6133c772a0fa932eb7d8d694d90
Lucas Wiman <lucaswiman@counsyl.com>
2015-01-01 15:51:51 -0800

    Fix lazy primes list.

--- prolog/primes.pl
+++ prolog/primes.pl
@@ -1,17 +1,18 @@
 :- dynamic
-  consecutive_primes/2
+  consecutive_primes/2,
+  is_prime/1
 .
 
-first_nontrivial_odd_divisor(K, Div, Test) :-
-  0 is K mod Test -> Div is Test;
+first_nontrivial_divisor(K, Div, Test) :-
   Test * Test > K -> false;
-  consecutive_primes(Test, Next), first_nontrivial_odd_divisor(K, Div, Next)
+  0 is K mod Test -> Div is Test;
+  consecutive_primes(Test, Next), first_nontrivial_divisor(K, Div, Next)
 .
 
 is_prime(K) :- 
-  K is 2;
-  not(0 is K mod 2), not(first_nontrivial_odd_divisor(K, _, 3));
-  false
+  not(first_nontrivial_divisor(K, _, 2)),
+  X is K,
+  asserta(is_prime(X))
 .
 
 consecutive_primes_helper(Prime1, Prime2, Test) :-
@@ -24,8 +25,15 @@ consecutive_primes(Prime1, Prime2) :-
   consecutive_primes_helper(Prime1, Prime2, Prime1 + 2)
 .
 
-primes_list([X, Y|Xs]) :-
-  freeze(Xs, (consecutive_primes(X, Y), primes_list_helper([Y|Xs])))
+primes_list_helper(X, Primes) :-
+  freeze(Primes, (
+      Primes=[Y|NextPrimes],
+      consecutive_primes(X, Y),
+      primes_list_helper(Y, NextPrimes)
+    )
+  )
 .
 
-primes_list(AllPrimes), append([2], _, AllPrimes).
+primes_list([2|Primes]) :-
+  primes_list_helper(2, Primes)
+.
