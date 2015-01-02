d6d4b974d70fdd84e7c1e0ebb1ad114a031a1e30
Lucas Wiman <lucaswiman@counsyl.com>
2015-01-01 17:22:27 -0800

    Problem 8

--- prolog/problem8.pl
+++ prolog/problem8.pl
@@ -21,3 +21,44 @@
 % 05886116467109405077541002256983155200055935729725
 % 71636269561882670428252483600823257530420752963450
 % Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+:- use_module(library(readutil)).
+:- use_module(library(apply)).
+
+code_list_to_number_list([], []).
+code_list_to_number_list([Code|CodeList], [Number|NumberList]) :-
+  number_codes(Number, [Code]),
+  code_list_to_number_list(CodeList, NumberList)
+.
+
+read_file_to_number_list(Filename, Numbers) :-
+  open(Filename, read, Stream),
+  read_stream_to_codes(Stream, Codes),
+  code_list_to_number_list(Codes, Numbers)
+.
+
+product_list([], 1).
+product_list([X|Xs], Result) :-
+  product_list(Xs, SubResult),
+  Result is X * SubResult
+.
+
+windows(_, 0, []) :- !.
+windows(List, WindowSize, Results) :-
+  append(Window, _, List), length(Window, WindowSize),
+  List=[_|SubList],
+  windows(SubList, WindowSize, NextResults)
+    -> Results=[Window|NextResults]
+  ; Results=[]
+.
+
+
+problem8(FileName, WindowSize, Max) :-
+  read_file_to_number_list(FileName, Numbers),
+  windows(Numbers, WindowSize, Windows),
+  maplist(product_list, Windows, Products),
+  max_list(Products, Max)
+.
+
+% problem8("./problem8.data", 4, Max), writeln(Max).
+% problem8("./problem8.data", 13, Max), writeln(Max).
