572fd65e5289b6733f53ef4b49f99a90c13f783b
Lucas Wiman <lucaswiman@counsyl.com>
2015-01-01 17:58:49 -0800

    Problem 9

---
+++ prolog/problem9.pl
@@ -0,0 +1,24 @@
+% A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+%
+% a2 + b2 = c2
+% For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+%
+% There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+% Find the product abc.
+
+pythagorean_triple_sum(Sum, Pair) :-
+  Pair=[A,B],
+  between(1, Sum, A),
+  between(A, Sum, B),
+  C is round(sqrt(A*A + B*B)),
+  0 is A*A + B*B - C*C,
+  Sum is A + B + C
+.
+
+problem9(Sum, Result) :-
+  findall(Pair, pythagorean_triple_sum(Sum, Pair), [[A,B]]),
+  C is round(sqrt(A*A + B*B)),
+  Result is A*B*C,
+  writeln([A, B, C]),
+  writeln(Result)
+.
