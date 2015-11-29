86494f9e140fcbdd58a59c9d4a1c7a76116dad23
Lucas Wiman <lucas.wiman@gmail.com>
2015-11-28 18:09:48 -0800

    Buggy grammar

---
+++ examples/lambda_calculus.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, unicode_literals
+import operator
+
+from parsimonious.grammar import Grammar
+from parsimonious.nodes import NodeVisitor
+
+from . import run_examples
+
+LAMBDA_CALCULUS = Grammar('''
+    inline_block = function (space arg)* ":" space? expr+
+    space = ~"[^\S\n]+"
+    expr = "(" expr+ ")" / name
+    function = ~"[A-Z][_a-zA-Z]*"
+    name = ~"[_a-zA-Z]+"
+''')
+
+LAMBDA_EXAMPLES = (
+    'PackageState num cont: cont num',
+)
+LAMBDA_NON_EXAMPLES = (
+    'PackageState num cont:',
+)
+
+if __name__ == "__main__":
+    run_examples(LAMBDA_CALCULUS, LAMBDA_EXAMPLES, LAMBDA_NON_EXAMPLES)
