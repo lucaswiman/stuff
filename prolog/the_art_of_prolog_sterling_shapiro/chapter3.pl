0e174a7146721d5812f8a3736f1c444e55644949
Lucas Wiman <lucas.wiman@gmail.com>
2015-12-06 15:38:35 -0800

    Update card handling, and get tests passing (with choicepoint)

--- prolog/the_art_of_prolog_sterling_shapiro/chapter3.pl
+++ prolog/the_art_of_prolog_sterling_shapiro/chapter3.pl
@@ -147,8 +147,6 @@ merge_sort([X|Xs], Y) :-
 %       Hints: ...
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-assert(suites = [hearts, diamonds, clubs, spades]).
-assert(values = [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]).
 
 not(index_of(_, [], 0)).
 index_of(Y, [X|Xs], Index) :-
@@ -156,9 +154,24 @@ index_of(Y, [X|Xs], Index) :-
   Y \= X -> (index_of(Y, Xs, Z), Index is Z+1)
 .
 
-worse_card(card(Value1, _), card(Value2, _)) :-
-  index_of(Value1, values, Index1),
-  index_of(Value2, values, Index2),
+card_values(Values) :- Values = [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace], !.
+card_suites(Suites) :- Suites = [hearts, diamonds, clubs, spades], !.
+
+valid_card(card(Value, Suite)) :-
+  card_values(Values), card_suites(Suites),
+  member(Value, Values), member(Suite, Suites)
+.
+
+card_index(card(Value, Suite), Index) :-
+  valid_card(card(Value, Suite)), card_values(Values),
+  index_of(Value, Values, Index)
+.
+
+suite(card(_, Suite), Suite) :- member(Suite, [hearts, diamonds, clubs, spades]).
+
+worse_card(Card1, Card2) :-
+  card_index(Card1, Index1),
+  card_index(Card2, Index2),
   Index1 =< Index2
 .
 
