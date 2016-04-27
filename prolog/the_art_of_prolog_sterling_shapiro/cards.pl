80e4d909e53554ea7611152fcf020a9b811d885a
Lucas Wiman <lucas.wiman@gmail.com>
2016-04-26 22:55:05 -0700

    stuff

--- prolog/the_art_of_prolog_sterling_shapiro/cards.pl
+++ prolog/the_art_of_prolog_sterling_shapiro/cards.pl
@@ -1,13 +1,26 @@
 :- use_module(library(clpfd)).
 
-is_card(card(Value, Suite)) :-
-  Value #>= 0, Value #< 12,
-  Suite #>= 0, Suite #< 4,
-  writeln([Value, Suite])
+is_card([Value, Suite]) :-
+  between(1, 13, Value),
+  between(1, 4, Suite)
 .
 
 hand(Cards) :-
-  sort(Cards, Cards),
   length(Cards, 5),
-  maplist(is_card, Cards)
+  maplist(is_card, Cards),
+  sort(Cards, Cards)
 .
+
+value_groups([V1, S1], [1, V1, [[V1, S1]]]).
+value_groups([[V1, S1], [V1, S2]|Xs], )
+
+:- begin_tests(is_card).
+  test(is_card) :-
+    findall(X, is_card(X), Deck),
+    length(Deck, 52)  % A deck should have 52 cards
+  .
+:- end_tests(is_card).
+
+:- begin_tests(hand).
+  test(hand) :- hand([[1,2], [2,2], [3,2], [4,2], [5,2]]).
+:- end_tests(hand).
