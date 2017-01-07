20f171d2dc75ff49b1765a9b0916b09a12c1eed9
Lucas Wiman <lucas.wiman@gmail.com>
2017-01-06 21:34:38 -0800

    Simplify

--- python/pygraphviz_bug/bug.py
+++ python/pygraphviz_bug/bug.py
@@ -4,27 +4,21 @@ import os
 import pygraphviz
 
 def doit():
-    A = pygraphviz.AGraph(name='', strict=False, directed=True)
+    A = pygraphviz.AGraph(strict=False, directed=True)
 
     A.add_node('a')
     A.add_node('b')
 
-    edges = [
-        ('a', 'b', 0, {}),
-        ('a', 'b', 1, {}),
-    ]
-    for u,v,key,edgedata in edges:
-        str_edgedata=dict((k,str(v)) for k,v in edgedata.items())
-        A.add_edge(u,v,key=str(key),**str_edgedata)
+    A.add_edge('a','b', key='0', label='0')
+    A.add_edge('a','b', key='1', label='1')
+
     os.system('rm /tmp/foo.dot /tmp/foo.png')
     A.write('/tmp/foo.dot')
     os.system('dot -Tpng /tmp/foo.dot -o /tmp/foo.png')
     os.system('open /tmp/foo.png')
-    edges_to_epsilon = [edge for edge in edges if edge[0] == 'a' and edge[1] == 'b']
-    A_edges_to_epsilon = [edge for edge in A.edges() if edge[0] == 'a' and edge[1] == 'b']
-    return A, (len(edges_to_epsilon) == len(A_edges_to_epsilon)), edges_to_epsilon, A_edges_to_epsilon
+    return A
 
 
-A, lengths_match, edges_orig, edges_A = doit()
-print((edges_orig, edges_A))
-assert lengths_match
+A = doit()
+print(len(A.edges()))
+assert len(A.edges()) == 2
