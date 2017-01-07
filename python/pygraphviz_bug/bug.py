41a8c6993de7da744f9d05af61e1d453eea531fe
Lucas Wiman <lucas.wiman@gmail.com>
2017-01-06 22:16:29 -0800

    Further reduce.

--- python/pygraphviz_bug/bug.py
+++ python/pygraphviz_bug/bug.py
@@ -3,22 +3,18 @@
 import os
 import pygraphviz
 
-def doit():
-    A = pygraphviz.AGraph(strict=False, directed=True)
+A = pygraphviz.AGraph(strict=False, directed=True)
 
-    A.add_node('a')
-    A.add_node('b')
+A.add_node('a')
+A.add_node('b')
 
-    A.add_edge('a','b', key='0', label='0')
-    A.add_edge('a','b', key='1', label='1')
+A.add_edge('a','b', key='0')
+A.add_edge('a','b', key='1')
 
-    os.system('rm /tmp/foo.dot /tmp/foo.png')
-    A.write('/tmp/foo.dot')
-    os.system('dot -Tpng /tmp/foo.dot -o /tmp/foo.png')
-    os.system('open /tmp/foo.png')
-    return A
+print(len(A.edges(keys=True)))
+assert len(A.edges(keys=True)) == 2
 
-
-A = doit()
-print(len(A.edges()))
-assert len(A.edges()) == 2
+os.system('rm /tmp/foo.dot /tmp/foo.png')
+A.write('/tmp/foo.dot')
+os.system('dot -Tpng /tmp/foo.dot -o /tmp/foo.png')
+os.system('open /tmp/foo.png')
