e58084efc6d645e18d100b5e018f2d5be605aff5
Lucas Wiman <lucas.wiman@gmail.com>
2017-05-06 13:11:36 -0700

    Concatenation and literals.

---
+++ python/simple_parser/simple_parser/__init__.py
@@ -0,0 +1,118 @@
+from collections import Iterable, namedtuple
+
+
+class Rule(object):
+    __slots__ = ()
+    def matches_at_position(self, string, position):
+        """
+        Returns a generator of matches in the string which start at position.
+        """
+        raise NotImplementedError
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, other):
+        return Concatenation(self, other)
+
+
+class Match(namedtuple('Match', ('string', 'position', 'length', 'rule', 'children'))):
+    def __new__(cls, string, position, length, rule=None, children=()):
+        return super(Match, cls).__new__(cls, string, position, length, rule, children)
+
+    def __str__(self):
+        return self.string[self.position:self.position + self.length]
+
+
+class _Epsilon(Rule):
+    __slots__ = ()
+    def __new__(cls):
+        try:
+            return Epsilon
+        except NameError:
+            return super(_Epsilon, cls).__new__(cls)
+
+    def __hash__(self):
+        return hash(self.__class__)
+
+    def matches_at_position(self, string, position):
+        # consumes 0 characters whatever the string
+        yield Match(string, position, 0, rule=self)
+
+    def __add__(self, other):
+        return other
+
+    def __radd__(self, other):
+        return other
+
+
+Epsilon = _Epsilon()
+
+
+class Literal(Rule):
+    __slots__ = ('literal', 'length')
+
+    def __init__(self, literal):
+        self.literal = literal
+        self.length = len(literal)
+
+    def __hash__(self):
+        return hash(self.literal)
+
+    def __eq__(self, other):
+        return isinstance(other, Literal) and other.literal == self.literal
+
+    def __repr__(self):
+        return 'Literal(%r)' % self.literal
+
+    def matches_at_position(self, string, position):
+        if string.startswith(self.literal, position):
+            yield Match(string, position, self.length, rule=self)
+
+
+class Concatenation(Rule):
+    __slots__ = ('head', 'tail')
+    def __new__(cls, *args):
+        if not args:
+            return Epsilon
+        elif len(args) == 1:
+            return args[0]
+        else:
+            concat = super(Concatenation, cls).__new__(cls)
+            concat.head = args[0]
+            concat.tail = Concatenation(*args[1:])
+            return concat
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Concatenation) and
+            self.head == other.head and
+            self.tail == other.tail)
+
+    def __iter__(self):
+        cur = self
+        while isinstance(cur, Concatenation):
+            yield cur.head
+            cur = cur.tail
+        yield cur
+
+    def __repr__(self):
+        return 'Concatenation(%s)' % ', '.join(map(repr, self))
+
+    def __add__(self, other):
+        if isinstance(other, Concatenation):
+            return Concatenation(*(tuple(self) + tuple(other)))
+        else:
+            return Concatenation(*(tuple(self) + (other, )))
+
+    def matches_at_position(self, string, position):
+        for match in self.head.matches_at_position(string, position):
+            for tail_match in self.tail.matches_at_position(string, position + match.length):
+                children = (match, ) + (tail_match.children if tail_match.children else (tail_match, ))
+                yield Match(
+                    string,
+                    position,
+                    length=match.length + tail_match.length,
+                    children=children,
+                    rule=self,
+                )
--- python/simple_parser/tests.py
+++ python/simple_parser/tests.py
@@ -1,2 +1,11 @@
-def test_the_thing():
-    assert False
+from simple_parser import Literal as L, Concatenation, Match
+
+def test_match():
+    rule = Concatenation(L('abc'), L('123'))
+    s = 'abc123'
+    assert list(rule.matches_at_position(s, 0)) == [
+        Match(s, 0, 6,
+              rule=rule,
+              children=(Match(s, 0, 3, L('abc')), Match(s, 3, 3, L('123')))
+        )
+    ]
