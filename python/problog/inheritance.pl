17286391b35effca7e55fe5e6b04c83fe360ca67
Lucas Wiman <lucas.wiman@gmail.com>
2017-07-15 10:50:23 -0700

    simplify

--- python/problog/inheritance.pl
+++ python/problog/inheritance.pl
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Facts
+% Observations
 observed_deleterious_count(mother, 1).
 observed_deleterious_count(father, 0).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -10,22 +10,18 @@ observed_deleterious_count(father, 0).
 1.0-0.9::deleterious(mother, chr_X1).
 1.0-0.9::deleterious(mother, chr_X2).
 
+0.5::transmitted(father, chr_X); 0.5::transmitted(father, chr_Y).
+0.5::transmitted(mother, chr_X1); 0.5::transmitted(mother, chr_X2).
+
 deleterious(mother, chr_X1) :- observed_deleterious_count(mother, Count), Count >= 1.
 deleterious(mother, chr_X2) :- observed_deleterious_count(mother, Count), Count >= 2.
 deleterious(father, chr_X) :- observed_deleterious_count(father, Count), Count >= 1.
 
 affected :-
-  (passed_deleterious(mother), transmitted(father, chr_Y));
-  (passed_deleterious(mother), passed_deleterious(father))
-.
+  passed_deleterious(mother), (transmitted(father, chr_Y); passed_deleterious(father)).
 
 passed_deleterious(Parent) :-
   (deleterious(Parent, X), transmitted(Parent, X)).
-
-transmitted(father, chr_X) :- \+transmitted(father, chr_Y).
-transmitted(mother, chr_X1) :- \+transmitted(mother, chr_X2).
-0.5::transmitted(father, chr_X); 0.5::transmitted(father, chr_Y).
-0.5::transmitted(mother, chr_X1); 0.5::transmitted(mother, chr_X2).
 
 foo :- transmitted(mother, chr_X1), transmitted(mother, chr_X2).
 foo2 :- transmitted(father, chr_X), transmitted(father, chr_Y).
