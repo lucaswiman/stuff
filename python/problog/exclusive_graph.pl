128a7f80eb6e80758a8e0ccecb8d464af8695a65
Lucas Wiman <lucas.wiman@gmail.com>
2017-07-15 10:27:40 -0700

    asdf

--- python/problog/exclusive_graph.pl
+++ python/problog/exclusive_graph.pl
@@ -1,12 +1,14 @@
-
-% (detection_rate = 0.9)
-1.0-0.9::deleterious(Parent, Chromosome).
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Facts
-observed_deleterious_count(mother, 2).
-observed_deleterious_count(father, 1).
+observed_deleterious_count(mother, 1).
+observed_deleterious_count(father, 0).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% (detection_rate = 0.9)
+0.0::deleterious(father, chr_Y).
+1.0-0.9::deleterious(father, chr_X).
+1.0-0.9::deleterious(mother, chr_X1).
+1.0-0.9::deleterious(mother, chr_X2).
 
 deleterious(mother, chr_X1) :- observed_deleterious_count(mother, Count), Count >= 1.
 deleterious(mother, chr_X2) :- observed_deleterious_count(mother, Count), Count >= 2.
@@ -21,20 +23,23 @@ passed_deleterious(mother) :-
   (deleterious(mother, chr_X1), transmitted(mother, chr_X1));
   (deleterious(mother, chr_X2), transmitted(mother, chr_X2)).
 
-:- transmitted(father, chr_X), \+transmitted(father, chr_Y); \+transmitted(father, chr_X), transmitted(father, chr_Y).
-:- transmitted(mother, chr_X1), \+transmitted(mother, chr_X2); \+transmitted(mother, chr_X1), transmitted(mother, chr_X2).
-0.5::transmitted(father, chr_X).
-0.5::transmitted(father, chr_Y).
-0.5::transmitted(mother, chr_X1).
-0.5::transmitted(mother, chr_X2).
+transmitted(father, chr_X) :- \+transmitted(father, chr_Y).
+transmitted(mother, chr_X1) :- \+transmitted(mother, chr_X2).
+0.5::transmitted(father, chr_X); 0.5::transmitted(father, chr_Y).
+0.5::transmitted(mother, chr_X1); 0.5::transmitted(mother, chr_X2).
 
 foo :- transmitted(mother, chr_X1), transmitted(mother, chr_X2).
+foo2 :- transmitted(father, chr_X), transmitted(father, chr_Y).
+
+query(foo).
+query(foo2).
 query(passed_deleterious(father)).
 query(passed_deleterious(mother)).
+query(deleterious(father, chr_Y)).
+query(deleterious(father, chr_X)).
 query(deleterious(mother, chr_X1)).
 query(deleterious(mother, chr_X2)).
 query(transmitted(mother, chr_X1)).
-query(foo).
 query(transmitted(mother, chr_X1)).
 query(transmitted(mother, chr_X2)).
 query(affected).  % Should be 100%, not 0.375
