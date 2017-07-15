9c09b84b999ffb4fb2d7ea19a0485223be881745
Lucas Wiman <lucas.wiman@gmail.com>
2017-07-14 22:56:54 -0700

    asdf

--- python/problog/exclusive_graph.pl
+++ python/problog/exclusive_graph.pl
@@ -12,18 +12,29 @@ deleterious(mother, chr_X1) :- observed_deleterious_count(mother, Count), Count 
 deleterious(mother, chr_X2) :- observed_deleterious_count(mother, Count), Count >= 2.
 deleterious(father, chr_X) :- observed_deleterious_count(father, Count), Count >= 1.
 
-affected :- passed_deleterious(mother), transmitted(father, chr_Y); passed_deleterious(mother), passed_deleterious(father).
+affected :-
+  (passed_deleterious(mother), transmitted(father, chr_Y));
+  (passed_deleterious(mother), passed_deleterious(father))
+.
 passed_deleterious(father) :- deleterious(father, chr_X), transmitted(father, chr_X).
-passed_deleterious(mother) :- deleterious(mother, chr_X1), transmitted(mother, chr_X1); deleterious(mother, chr_X2), transmitted(mother, chr_X2).
+passed_deleterious(mother) :-
+  (deleterious(mother, chr_X1), transmitted(mother, chr_X1));
+  (deleterious(mother, chr_X2), transmitted(mother, chr_X2)).
+
 :- transmitted(father, chr_X), \+transmitted(father, chr_Y); \+transmitted(father, chr_X), transmitted(father, chr_Y).
 :- transmitted(mother, chr_X1), \+transmitted(mother, chr_X2); \+transmitted(mother, chr_X1), transmitted(mother, chr_X2).
-
 0.5::transmitted(father, chr_X).
 0.5::transmitted(father, chr_Y).
 0.5::transmitted(mother, chr_X1).
-0.5::transmitted(father, chr_X2).
+0.5::transmitted(mother, chr_X2).
 
-
+foo :- transmitted(mother, chr_X1), transmitted(mother, chr_X2).
 query(passed_deleterious(father)).
 query(passed_deleterious(mother)).
+query(deleterious(mother, chr_X1)).
+query(deleterious(mother, chr_X2)).
+query(transmitted(mother, chr_X1)).
+query(foo).
+query(transmitted(mother, chr_X1)).
+query(transmitted(mother, chr_X2)).
 query(affected).  % Should be 100%, not 0.375
